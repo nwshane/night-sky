b9f99c694d534dde70cca995a77cd3f4b7d6d41c
nwshane <django09@gmail.com>
2020-04-29 12:52:48 -0400

    Added some points

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -6,4 +6,30 @@ function setup() {
 
 function draw() {
   background(50);
+
+  translate(width / 2, height / 3);
+  stroke("purple");
+  strokeWeight(10);
+  point(0, 0);
+
+  push();
+  stroke("blue");
+  strokeWeight(5);
+  rotate(frameCount / 200);
+  point(width / 4, height / 4);
+  pop();
+
+  push();
+  stroke("blue");
+  strokeWeight(5);
+  rotate(frameCount / 200);
+  point(width / -8, height / 4);
+  pop();
+
+  push();
+  stroke("blue");
+  strokeWeight(5);
+  rotate(frameCount / 200);
+  point(width / -2, height / -4);
+  pop();
 }
