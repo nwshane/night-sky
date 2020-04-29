4674eff81aecbfb30a597e8886f9e399f93a1907
nwshane <django09@gmail.com>
2020-04-29 13:16:27 -0400

    Made point be generated at a random location

--- build/build.js
+++ build/build.js
@@ -3,29 +3,35 @@ var squares = 10;
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
+var getNorthStarX = function () { return width / 2; };
+var getNorthStarY = function () { return height / 3; };
+function getRandomIntBetween(min, max) {
+    var difference = max - min;
+    return Math.floor(Math.random() * Math.floor(difference) + min);
+}
+var randomlyGeneratedAreaModifier = 0.5;
+function chooseRandomXOnPage() {
+    return getRandomIntBetween(-1 * randomlyGeneratedAreaModifier * getNorthStarX(), randomlyGeneratedAreaModifier * getNorthStarX());
+}
+function chooseRandomYOnPage() {
+    return getRandomIntBetween(-1 * randomlyGeneratedAreaModifier * getNorthStarY(), randomlyGeneratedAreaModifier * getNorthStarY());
+}
+var starX, starY;
 function draw() {
     background(50);
-    translate(width / 2, height / 3);
+    translate(getNorthStarX(), getNorthStarY());
     stroke("purple");
     strokeWeight(10);
     point(0, 0);
+    if (!starX)
+        starX = chooseRandomXOnPage();
+    if (!starY)
+        starY = chooseRandomYOnPage();
     push();
     stroke("blue");
-    strokeWeight(5);
+    strokeWeight(20);
     rotate(frameCount / 200);
-    point(width / 4, height / 4);
-    pop();
-    push();
-    stroke("blue");
-    strokeWeight(5);
-    rotate(frameCount / 200);
-    point(width / -8, height / 4);
-    pop();
-    push();
-    stroke("blue");
-    strokeWeight(5);
-    rotate(frameCount / 200);
-    point(width / -2, height / -4);
+    point(starX, starY);
     pop();
 }
 //# sourceMappingURL=build.js.map
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -4,32 +4,62 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+const getNorthStarX = () => width / 2;
+const getNorthStarY = () => height / 3;
+
+function getRandomIntBetween(min, max) {
+  const difference = max - min;
+  return Math.floor(Math.random() * Math.floor(difference) + min);
+}
+
+const randomlyGeneratedAreaModifier = 0.5;
+
+// between the left side of the page and the right side of the page
+function chooseRandomXOnPage() {
+  return getRandomIntBetween(
+    -1 * randomlyGeneratedAreaModifier * getNorthStarX(),
+    randomlyGeneratedAreaModifier * getNorthStarX()
+  );
+}
+
+function chooseRandomYOnPage() {
+  return getRandomIntBetween(
+    -1 * randomlyGeneratedAreaModifier * getNorthStarY(),
+    randomlyGeneratedAreaModifier * getNorthStarY()
+  );
+}
+
+let starX, starY;
+
 function draw() {
   background(50);
 
-  translate(width / 2, height / 3);
+  translate(getNorthStarX(), getNorthStarY());
   stroke("purple");
   strokeWeight(10);
   point(0, 0);
 
-  push();
-  stroke("blue");
-  strokeWeight(5);
-  rotate(frameCount / 200);
-  point(width / 4, height / 4);
-  pop();
+  if (!starX) starX = chooseRandomXOnPage();
+  if (!starY) starY = chooseRandomYOnPage();
 
   push();
   stroke("blue");
-  strokeWeight(5);
+  strokeWeight(20);
   rotate(frameCount / 200);
-  point(width / -8, height / 4);
+  point(starX, starY);
   pop();
 
-  push();
-  stroke("blue");
-  strokeWeight(5);
-  rotate(frameCount / 200);
-  point(width / -2, height / -4);
-  pop();
+  //   push();
+  //   stroke("blue");
+  //   strokeWeight(5);
+  //   rotate(frameCount / 200);
+  //   point(width / -8, height / 4);
+  //   pop();
+
+  //   push();
+  //   stroke("blue");
+  //   strokeWeight(5);
+  //   rotate(frameCount / 200);
+  //   point(width / -2, height / -4);
+  //   pop();
 }
