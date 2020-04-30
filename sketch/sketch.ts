0697933f495eaf3c484169dcf98cec8f60a9fc79
nwshane <django09@gmail.com>
2020-04-30 12:00:22 -0400

    Change method of positioning stars randomly

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,54 +1,51 @@
 let angle = 0;
 let squares = 10;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  console.log({ windowWidth, windowHeight });
 }
 
-const getNorthStarX = () => width / 2;
-const getNorthStarY = () => height / 3;
+const getNorthStarX = () => width / 10;
+const getNorthStarY = () => height / 10;
 
 function getRandomIntBetween(min: number, max: number) {
   const difference = max - min;
   return Math.floor(Math.random() * Math.floor(difference) + min);
 }
 
-// This modifier constant modifies the space in which a star can be generated.
-// If it is set to 1, then a star can be generated anywhere within the page. However,
-// the problem with only generating stars within the area of the page is that when they rotate,
-// blank parts farther away from the north star will appear that were not present on the
-// screen beforehand.
-// The way to fix this would be to generate stars in relation to the North Star, I think, and to
-// allow them to be generated as far away as the north star is distant from a corner of the page.
-const AreaModifier = 2;
+function getRandomXRelativeToNorthStar() {
+  const northStarX = getNorthStarX();
 
-// between the left side of the page and the right side of the page
-function chooseRandomXOnPage() {
-  return getRandomIntBetween(
-    -1 * AreaModifier * getNorthStarX(),
-    AreaModifier * (width - getNorthStarX())
-  );
+  // Horizontal distance from north star to most distant side of page.
+  const xMaxDistance = Math.max(northStarX, width - northStarX);
+
+  return getRandomIntBetween(-1 * xMaxDistance, xMaxDistance);
 }
 
-function chooseRandomYOnPage() {
-  return getRandomIntBetween(
-    -1 * AreaModifier * getNorthStarY(),
-    AreaModifier * (height - getNorthStarY())
-  );
+function getRandomYRelativeToNorthStar() {
+  const northStarY = getNorthStarY();
+
+  // Vertical distance from north star to either bottom or top of page,
+  // whichever is farther away.
+  const yMaxDistance = Math.max(northStarY, height - northStarY);
+
+  return getRandomIntBetween(-1 * yMaxDistance, yMaxDistance);
 }
 
 interface Star {
-  x: number;
-  y: number;
+  startingXRelativeToNorthStar: number;
+  startingYRelativeToNorthStar: number;
   diameter: number;
 }
 
 const createStar = (): Star => ({
-  x: chooseRandomXOnPage(),
-  y: chooseRandomYOnPage(),
+  startingXRelativeToNorthStar: getRandomXRelativeToNorthStar(),
+  startingYRelativeToNorthStar: getRandomYRelativeToNorthStar(),
   diameter: getRandomIntBetween(1, 10),
 });
 
-const starCount = 50;
+const starCount = 500;
 const stars: Star[] = [];
 
 function draw() {
@@ -60,11 +57,12 @@ function draw() {
   point(0, 0);
 
   // only run the create star code once
-  if (!stars[0])
+  if (!stars[0]) {
     // create stars
     for (let i = 0; i < starCount; i++) {
       stars.push(createStar());
     }
+  }
 
   // render stars
   for (const star of stars) {
@@ -72,7 +70,7 @@ function draw() {
     stroke("blue");
     strokeWeight(star.diameter);
     rotate(frameCount / 200);
-    point(star.x, star.y);
+    point(star.startingXRelativeToNorthStar, star.startingYRelativeToNorthStar);
     pop();
   }
 }
