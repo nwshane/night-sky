036792f8d990d48471ad5f03bd952bf78cbddcf8
nwshane <django09@gmail.com>
2020-05-04 12:45:18 -0400

    Remove roby todo

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -7,7 +7,6 @@ const StarColor = "rgba(249, 255, 0, 0.4)";
 
 /*
 todo:
-- from Roby: I think something that could help with depth is making the smaller circles more transparent and maybe making the background a more intense hue, perhaps a dark blue like #001B44 - from there try playing with colors for the stars. I like using this palette as a nice reference for colors that pair well together on a screen http://tachyons.io/docs/themes/skins/ :)
 - make stars twinkle
 - add Earth
 */
